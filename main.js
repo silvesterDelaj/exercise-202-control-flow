d36f76b96bf2874d166331f524dd5e9f9a8d0a2b
Silvester <silvesterdelaj@gmail.com>
2023-12-02 18:28:51 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+
+// -----exercise 6------
+
+// const currentAge = 44;
+// const retirementDistance = 65 - currentAge; 
+// if (currentAge < 65) {
+//     alert (`You have ${retirementDistance} years left until retirement.`);
+// } else {
+//     alert (`You are eligible for retirement.`);
+// }
+
+
+// -------exercise 7-------
+
+// let distanceToTravel = 100;
+// let travelBudget = 199;
+// let rateKm = 2;
+// let TravelCost = distanceToTravel * rateKm;
+// if (TravelCost > travelBudget) {
+//     alert (`Please, increase your travel budget or choose a closer destination.`);
+// };
+
+
+// --------exercise 8---------
+
+// let celsiusTemperature = 10;
+// let fahrenheitTemperature = ((celsiusTemperature * 9/5) + 32);
+// alert (`Current temperature is ${fahrenheitTemperature}F`);
+
+
+// --------exercise 9---------
+
+// let currentSavings = 5000;
+// let savingsGoal = 50000;
+// let savingsNeeded = savingsGoal - currentSavings;
+// if (currentSavings < savingsGoal) {
+//     alert (`You still need to save ${savingsNeeded}$. You're fucking poor`);
+// } else {
+//     alert (`Amazing, you're officially a TOP G.`);
+// };
+
+// --------exercise 10---------
+
+// let numberOfWords = 1000;
+// let readingTime = 4;
+// let wordsPerMinute = numberOfWords / readingTime;
+// if (wordsPerMinute < 200) {
+//     alert (`Your reading speed is ${wordsPerMinute}. You're too slow, please improve.`);
+// } else if (wordsPerMinute > 300){
+//     alert (`Your reading speed is ${wordsPerMinute}. You're exceptionally fast.`);
+// } else {
+//     alert (`Your reading speed is ${wordsPerMinute}.`)
+// }
